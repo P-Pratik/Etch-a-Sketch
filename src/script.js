41eef5290e22e70140f034c7f778951b132d6bf3
P-Pratik <114237737+P-Pratik@users.noreply.github.com>
2023-07-23 19:59:10 +0530

    feature addition Eraser Color Selection

--- src/script.js
+++ src/script.js
@@ -1,4 +1,8 @@
 let boxGrid = document.querySelector('#boxgrid');
+let color = 'black';
+
+let eraser = document.querySelector('#eraser');
+let colorSelection = document.querySelector('#colorSelect');
 
 for(let i = 0; i < 16 ; i++ ){
     let rowElement = document.createElement('div');
@@ -8,8 +12,27 @@ for(let i = 0; i < 16 ; i++ ){
     for(let j =0 ; j < 16 ; j++){
         let columnElement = document.createElement('div');
         columnElement.classList.add('column');
-        columnElement.setAttribute('id', 'column')
+        columnElement.setAttribute('id', 'column');
         rowElement.append(columnElement);
     }
 }
 
+function eventListenerById(className , event, color ){
+    let element = document.querySelectorAll(className);
+    for (let i = 0; i < element.length; i++){
+        element[i].addEventListener(event, () => {
+            element[i].style.backgroundColor = color ;
+        });
+    }
+}
+
+eraser.addEventListener('click', () => {
+    color = 'white';
+    eventListenerById('#column','mousemove', color);
+});
+
+colorSelection.addEventListener('input' , () => {
+    color = colorSelection.value;
+    eventListenerById('#column', 'mousemove', color);
+});
+eventListenerById('#column','mousemove', color);
